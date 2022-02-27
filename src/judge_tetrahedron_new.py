69725b32ada2f7fc39d7e5c2e03ac4e00220f5f5
why <1136003685@qq.com>
2022-02-27 09:44:37 +0800

    [Release] use multiprocess to accerate the program

--- src/judge_tetrahedron_new.py
+++ src/judge_tetrahedron_new.py
@@ -8,42 +8,40 @@ from pymatgen.ext.matproj import MPRester
 from pymatgen.io.cif import CifParser
 import os
 import shutil
+from multiprocessing import Process
+
+multi_process_num = 3
 
 
-def judge_tetrahedron_csm(unit_cell_cif_address):
-    parser = CifParser(unit_cell_cif_address)
-    struct = parser.get_structures()[0]
+def start_filter(input_dir, file_lst, output_dir):
     lgf = LocalGeometryFinder()
     lgf.setup_parameters(centering_type='centroid', include_central_site_in_centroid=True)
-    lgf.setup_structure(structure=struct)
-    # se
-    se = lgf.compute_structure_environments(maximum_distance_factor=1.41, only_cations=False)
+    def judge_tetrahedron_csm(unit_cell_cif_address):
+        parser = CifParser(unit_cell_cif_address)
+        struct = parser.get_structures()[0]
+        lgf.setup_structure(structure=struct)
 
-    # strategy
-    strategy = SimplestChemenvStrategy(distance_cutoff=1.4, angle_cutoff=0.3)
-    lse = LightStructureEnvironments.from_structure_environments(strategy=strategy,
-                                                                 structure_environments=se)
-    print(len(struct))
-    atom_num = len(struct)
-    for isite in range(atom_num):
-        type = lse.coordination_environments[isite][0]["ce_symbol"][0]
-        print(type)
-        if type != "T":
-            return False
-    return True
-
-
-def start_filter(input_dir="../data/unit_cell/",
-                 judge_function=judge_tetrahedron_csm,
-                 output_dir="../data/tetrahedron_126335_csm/"):
-    if not os.path.exists(output_dir):
-        raise ValueError("[Error] output dir does not exist")
-    file_lst = os.listdir(input_dir)
+        # se
+        se = lgf.compute_structure_environments(maximum_distance_factor=1.41, only_cations=False)
+        # strategy
+        strategy = SimplestChemenvStrategy(distance_cutoff=1.4, angle_cutoff=0.3)
+        lse = LightStructureEnvironments.from_structure_environments(strategy=strategy,
+                                                                     structure_environments=se)
+        print(len(struct))
+        atom_num = len(struct)
+        for isite in range(atom_num):
+            type = lse.coordination_environments[isite][0]["ce_symbol"][0]
+            print(type)
+            if type != "T":
+                return False
+        return True
     for i, file in enumerate(file_lst):
         try:
-            print("index = ", i)
+            if i < 4946 // multi_process_num:
+                continue
+            print("index = ", i, "file = ", file)
             address = input_dir + file
-            if judge_function(address):
+            if judge_tetrahedron_csm(address):
                 print("copy")
                 shutil.copyfile(address, output_dir + file)
         except Exception as e:
@@ -52,5 +50,15 @@ def start_filter(input_dir="../data/unit_cell/",
 
 
 if __name__ == "__main__":
-    start_filter(input_dir="../data/tetrahedron_124657_union_unit_cell/", judge_function=judge_tetrahedron_csm,
-                 output_dir="../data/tetrahedron_126335_csm/")
+    file_lst = os.listdir("../data/unit_cell/")
+    file_lst_lst = [[] for _ in range(multi_process_num)]
+    for k, file in enumerate(file_lst):
+        file_lst_lst[k % 3].append(file)
+    for i in range(multi_process_num):
+        print(len(file_lst_lst[i]))
+
+    for i in range(multi_process_num):
+        p = Process(target=start_filter, args=("../data/unit_cell/", file_lst_lst[i],
+                                               "../data/tetrahedron_126335_csm/"),
+                    name=f"work_{i}")
+        p.start()
